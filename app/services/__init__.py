951f1ab23a8fe86401743bf5624b9a810b54892d
deepak121 <thegreatdeepakpoudel@gmail.com>
2026-05-17 11:52:32 +0545

    fix import

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,4 +1,4 @@
 
-from .patient_service import get_or_create_patient, save_message
+from .patient_service import get_or_create_patient, save_message, get_conversation_history
 from .llm_service import llm_service, generate_reply
 from .scheduler import start_scheduler, stop_scheduler
